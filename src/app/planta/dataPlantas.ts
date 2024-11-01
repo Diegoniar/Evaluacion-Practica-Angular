b886f8205e1eb3aeff20195f82b0a4ad5420fae3
Diego Niño Arias <da.ninoa1@uniandes.edu.co>
2024-11-01 18:06:24 -0500

    Inclusión del modulo de plantas

---
+++ src/app/planta/dataPlantas.ts
@@ -0,0 +1,9 @@
+import { Planta } from "./planta";
+
+export const dataPlantas = [
+    new Planta(1,"llantén de agua","Alisma plantago-aquatica","Alismataceae",80,"Zonas Húmedas","zonas húmedas como colas de embalses, balsas y riberas fluviales."),
+    new Planta(1,"llantén de agua","Alisma plantago-aquatica","Alismataceae",80,"Zonas Húmedas","zonas húmedas como colas de embalses, balsas y riberas fluviales."),
+    new Planta(1,"llantén de agua","Alisma plantago-aquatica","Alismataceae",80,"Zonas Húmedas","zonas húmedas como colas de embalses, balsas y riberas fluviales."),
+    new Planta(1,"llantén de agua","Alisma plantago-aquatica","Alismataceae",80,"Zonas Húmedas","zonas húmedas como colas de embalses, balsas y riberas fluviales."),
+    new Planta(1,"llantén de agua","Alisma plantago-aquatica","Alismataceae",80,"Zonas Húmedas","zonas húmedas como colas de embalses, balsas y riberas fluviales.")
+]
--- src/app/planta/planta.component.ts
+++ src/app/planta/planta.component.ts
@@ -1,15 +1,23 @@
 import { Component, OnInit } from '@angular/core';
+import { Planta } from './planta';
+import { dataPlantas } from './dataPlantas';
 
 @Component({
   selector: 'app-planta',
   templateUrl: './planta.component.html',
   styleUrls: ['./planta.component.css']
 })
-export class PlantaComponent implements OnInit {
 
+export class PlantaComponent implements OnInit {
+  plantas: Array<Planta> = [];
   constructor() { }
 
+  getPlantasList(): Array<Planta>{
+    return dataPlantas;
+  }
+
   ngOnInit() {
+    this.plantas = this.getPlantasList();
   }
 
 }
